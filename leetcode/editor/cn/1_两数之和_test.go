2ce7103d0a9c35b056b9619fd84cb4163a268fea
zhanfeng.zhang <zhang.zzf@alibaba-inc.com>
2023-06-29 19:03:19 +0800

    feat: init go-leetcode proj

---
+++ leetcode/editor/cn/1_两数之和_test.go
@@ -0,0 +1,17 @@
+package leetcode
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_givenNormal_when_thenSuccess(t *testing.T) {
+	assert.Equal(t, 1, 1, "shouldEqual")
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func twoSum(nums []int, target int) []int {
+	return []int{}
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
