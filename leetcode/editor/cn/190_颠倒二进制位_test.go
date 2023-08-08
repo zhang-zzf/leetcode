24a5f0ba9e66982b598c047c44a4900b48b38191
zzf.zhang <zhangzhanfeng@icloud.com>
2023-08-08 09:55:48 +0800

    feat: update algorithm leetcode190

--- leetcode/editor/cn/190_颠倒二进制位_test.go
+++ leetcode/editor/cn/190_颠倒二进制位_test.go
@@ -17,10 +17,11 @@ func reverseBits(num uint32) uint32 {
 		M2 = 0x33333333
 		M1 = 0x55555555
 	)
-	num = num>>1&M1 | num&M1<<1
-	num = num>>2&M2 | num&M2<<2
-	num = num>>4&M4 | num&M4<<4
-	num = num>>8&M8 | num&M8<<8
+	// 使用括号后，不用考虑符号优先级
+	num = ((num >> 1) & M1) | ((num & M1) << 1)
+	num = ((num >> 2) & M2) | ((num & M2) << 2)
+	num = ((num >> 4) & M4) | ((num & M4) << 4)
+	num = ((num >> 8) & M8) | ((num & M8) << 8)
 	num = num>>16 | num<<16
 	return num
 }
