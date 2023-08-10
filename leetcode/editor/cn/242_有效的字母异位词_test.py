02d2aef1116ca143c6f24ecfb5e05de3762e82b3
zzf.zhang <zhangzhanfeng@icloud.com>
2023-08-10 12:31:56 +0800

    feat: update algorithm leetcode242
    思路：数组基数计算

--- leetcode/editor/cn/242_有效的字母异位词_test.py
+++ leetcode/editor/cn/242_有效的字母异位词_test.py
@@ -14,21 +14,15 @@ class TestSolution(unittest.TestCase):
 # leetcode submit region begin(Prohibit modification and deletion)
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        cnt_map: dict[str, int] = {}
+        cnt = [0] * 26
         for c in s:
-            cnt = cnt_map.get(c)
-            cnt_map[c] = 1 if cnt is None else cnt + 1
+            cnt[ord(c) - ord('a')] += 1
         for c in t:
-            cnt = cnt_map.get(c)
-            if cnt is None or cnt - 1 < 0:
+            cnt[ord(c) - ord('a')] -= 1
+        for c in cnt:
+            if c != 0:
                 return False
-            if cnt - 1 == 0:
-                del cnt_map[c]
-            else:
-                cnt_map[c] = cnt - 1
-        return len(cnt_map) == 0
+        return True
 
 
 # leetcode submit region end(Prohibit modification and deletion)
