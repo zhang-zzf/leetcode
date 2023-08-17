c79f799f7afe8a4d3ecb973b68cc5f6c96e79d66
zzf.zhang <zhangzhanfeng@icloud.com>
2023-08-17 14:25:23 +0800

    feat: update algorithm leetcode392
    思路：优化版的动态规划及双指针

--- leetcode/editor/cn/392_判断子序列_test.go
+++ leetcode/editor/cn/392_判断子序列_test.go
@@ -27,7 +27,61 @@ func Test_givenFailedCase2_when392_thenSuccess(t *testing.T) {
 }
 
 //leetcode submit region begin(Prohibit modification and deletion)
+/**
+isSubsequence2 的基础上优化
+*/
 func isSubsequence(s string, t string) bool {
+	//如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，
+	//你需要依次检查它们是否为 T 的子序列
+	// 分析 t 中的字母和顺序
+	// TODO 动态创建数组
+	m := len(t)
+	dp := make([][26]int, m+1)
+	// 填充边界
+	for i := 0; i < 26; i++ {
+		dp[m][i] = m
+	}
+	// O(26*m)
+	for i := m - 1; i >= 0; i-- {
+		for j := 0; j < 26; j++ {
+			if int(t[i]-'a') == j {
+				dp[i][j] = i
+			} else {
+				dp[i][j] = dp[i+1][j]
+			}
+		}
+	}
+	// 判断 s 字符串
+	i, j, n := 0, 0, len(s)
+	for ; i < n; i++ {
+		// watch out for the index
+		next := dp[j][s[i]-'a']
+		if next == m {
+			break
+		}
+		// core
+		j = next + 1
+	}
+	return i == n
+}
+
+/**
+相比较  isSubsequence1 更简洁
+*/
+func isSubsequence11(s string, t string) bool {
+	// 双指针
+	n, m := len(s), len(t)
+	i, j := 0, 0
+	for i < n && j < m {
+		if s[i] == t[j] {
+			i += 1
+		}
+		j += 1
+	}
+	return i == n
+}
+
+func isSubsequence2(s string, t string) bool {
 	//如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，
 	//你需要依次检查它们是否为 T 的子序列
 	// 分析 t 中的字母和顺序
